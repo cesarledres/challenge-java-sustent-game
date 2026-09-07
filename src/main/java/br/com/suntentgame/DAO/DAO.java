6a95202cf1621be3da91dfdd22afcb97671b8a01
cesarledres <cesarledrescv@gmail.com>
2026-09-07 19:17:03 -0300

    fix: muda o tipo do parametro do metodo de consulta da interface DAO.

--- src/main/java/br/com/suntentgame/DAO/DAO.java
+++ src/main/java/br/com/suntentgame/DAO/DAO.java
@@ -7,5 +7,5 @@ public interface DAO<T> {
     public boolean cadastrar(T entidade)   throws SQLException;
     public boolean atualizar(T entidade)  throws SQLException;
     public boolean remover(T entidade)  throws SQLException;
-    public Optional<T> consultarPoId(T entidade)  throws SQLException;
+    public Optional<T> consultarPoId(int entidade)  throws SQLException;
 }
