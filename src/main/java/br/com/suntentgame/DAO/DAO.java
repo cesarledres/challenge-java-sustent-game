1ba5e69da1b8d15aa7aa7964dcfb8a7a2a92287e
cesarledres <cesarledrescv@gmail.com>
2026-09-07 16:09:04 -0300

    feat: adiciona interface DAO com suas assinaturas

---
+++ src/main/java/br/com/suntentgame/DAO/DAO.java
@@ -0,0 +1,11 @@
+package br.com.suntentgame.DAO;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+public interface DAO<T> {
+    public boolean cadastrar(T entidade)   throws SQLException;
+    public boolean atualizar(T entidade)  throws SQLException;
+    public boolean remover(T entidade)  throws SQLException;
+    public Optional<T> consultarPoId(T entidade)  throws SQLException;
+}
